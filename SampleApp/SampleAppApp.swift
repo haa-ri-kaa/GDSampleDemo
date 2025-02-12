b6258a6a2a417959c4bb767e8131db50c7ab8d2a
Harika Rudraraju <hrudraraju@Harikas-MacBook-Pro.local>
2025-02-12 10:51:18 +0530

    sample app second commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Harika Rudraraju on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
